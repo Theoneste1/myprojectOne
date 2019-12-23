f27249b8270fe75c944c8338f91f4bec29fd4390
theoneste1 <theoneste99@gmail.com>
2019-12-23 18:12:09 +0200

    I am feeling better to deal with props

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
 import React,{Component}from 'react';
 import './App.css';
-import Myprofile from './components/Myprofile/Myprofile';
-
+// import Myprofile from './components/Myprofile/Myprofile';
+import Login from './components/login/Login';
 
 class App extends Component{
   
   render(){
     return (
       <div className="App"> 
-       <Myprofile fname="Theoneste" lname="yyyy"/>
-       <Myprofile fname="gggg" lname="zzzz"/>
-       <Myprofile fname="ffff" lname="jjjjj"/>
+       {/* <Myprofile fname="Theoneste" lname="yyyy"/> */}
+       {/* <Myprofile fname="gggg" lname="zzzz"/>
+       <Myprofile fname="ffff" lname="jjjjj"/> */}
+        <Login fname='Theoneste' lname='hhhh'/>
+        
+
+
       </div>
     );
   }
---
+++ src/components/login/Login.js
@@ -0,0 +1,33 @@
+import React,{Component} from 'react'
+import './Login.css'
+
+function Login(props){
+
+    return(
+        <div>
+            <div>Login page</div>
+        <div className="login">
+            <div className='login_info'>
+                <div className="picture">
+                    <div className='log'>Facebooksdfsdgdsgds</div>
+                    <div className='logname' >Facebook</div>
+
+                    
+                </div>
+                <div className="personalinfo">
+                    <h3>FirstName:{props.fname}</h3>
+                    <h3>LastName:{props.lname}</h3>
+                </div>
+            </div>
+
+            <div className="details">
+                <input type='text' placeholder='Enter your comment here:'></input>
+                <h3>The name interered:{props.fname + " "+props.lname}</h3>
+                <h3> Thank you man</h3>
+            </div>
+        </div>
+        </div>
+        
+    )
+}
+export default Login
